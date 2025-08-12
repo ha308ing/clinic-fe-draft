e1ea755c735ac06a1986c58225e0d2e9cf439900
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-08-12 11:13:51 +0400

    feat: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { createBrowserRouter } from "react-router";
+import { ROUTES } from "./constants.ts";
+import { RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: ROUTES.ROOT,
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
